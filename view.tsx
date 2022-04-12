e610b15624ebd0704347c2e77494eadaa6535527
Oscar Capraro <oscar@mcapraro.com>
2022-04-12 00:27:43 -0700

    fix: remove reactdom render

--- view.tsx
+++ view.tsx
@@ -3,11 +3,12 @@ import * as React from "react";
 import { useState } from "react";
 import * as ReactDOM from "react-dom";
 import { ExcalidrawCanvas } from "./ExcalidrawCanvas";
+import {createRoot} from 'react-dom/client';
 
 export const renderCanvas = (container: any, id: number, saveToFile:any) => {
   // const setHeight=(a:any)=>{return a}
-  ReactDOM.render(
-    <ExcalidrawCanvas id={id} saveToFile={saveToFile}  />,
-    container
+  let root = createRoot(container);
+  root.render(
+    <ExcalidrawCanvas id={id} saveToFile={saveToFile}  />
   );
 }
